4bbcd8122a82640942f807185de23c84bbba2011
Elena <zorenkoelen@gmail.com>
2022-02-14 00:22:39 +0200

    Update .github/workflows/__test__

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -6,12 +6,23 @@ import diff from '../src/diff.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+// const firstFile = getFixturePath('file1.json');
+//   const secondFile = getFixturePath('file2.json');
+//   const expectedAnswer = readFile('result.txt');
+//   const recevedAnswer = diff(firstFile, secondFile);
+
+// console.log('firstFile', firstFile);
+// console.log('secondFile', secondFile);
+// console.log('expectedAnswer', expectedAnswer);
+// console.log('recevedAnswer', recevedAnswer);
+
 test('diff', () => {
-  const firstFile = getFixturePath(file1.json);
-  const secondFile = getFixturePath(file2.json);
-  const expectedAnswer = readFile(result.txt);
+  const firstFile = getFixturePath('file1.json');
+  const secondFile = getFixturePath('file2.json');
+  const expectedAnswer = readFile('result.txt');
   const recevedAnswer = diff(firstFile, secondFile);
-  expect(expectedAnswer).toEqual(recevedAnswer);
+  expect(expectedAnswer).toBe(recevedAnswer);
 });
